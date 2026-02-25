3031b2b221b5b330329ca10a087c260006fcf548
davidsegle <davids.egle3@gmail.com>
2026-02-25 10:30:07 +0200

    Add: added variables and getters in student class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,28 @@
 package model;
 
 public class Student {
-
+	private long studId;
+	private String name;
+	private String surname;
+	private String personCode;
+	
+	public long getStudId() {
+		return studId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public String getPersonCode() {
+		return personCode;
+	}
+	
+	
+	
+	
 }
