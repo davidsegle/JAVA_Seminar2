c8b1fcdce2e1faf6126c8d1735681e2224e77a13
davidsegle <davids.egle3@gmail.com>
2026-02-25 11:17:29 +0200

    Added: get and set functions with regex

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,11 +1,15 @@
 package model;
 
 public class Student {
+	//mainigie
 	private long studId;
 	private String name;
 	private String surname;
 	private String personCode;
 	
+	private static long counter=0;
+	
+	//get funkcijas
 	public long getStudId() {
 		return studId;
 	}
@@ -22,7 +26,37 @@ public class Student {
 		return personCode;
 	}
 	
-	
+	//set funkcijas
+	public void setStudId() {
+		studId = counter;
+		counter++;
+	}
+	public void setName(String inputName) {
+		if((inputName !=null) && (!inputName.isEmpty()) && (inputName.matches("[A-ZĒŪĪĀŠĢĶĻŽČŅ]{1}[a-zēūīāšģķļžčņ]{2,15}( {1}[A-ZĒŪĪĀŠĢĶĻŽČŅ]{1}[a-zēūīāšģķļžčņ]{2,15})?"))   ) {
+			name = inputName;
+		}
+		else {
+			name = "Unknown";
+		}
+		
+	}
+	public void setSurname(String inputSurname) {
+		if((inputSurname != null) && (!inputSurname.isEmpty()) && (inputSurname.matches("[A-ZĒŪĪĀŠĢĶĻŽČŅ]{1}[a-zēūīāšģķļžčņ]{2,15}( {1}[A-ZĒŪĪĀŠĢĶĻŽČŅ]{1}[a-zēūīāšģķļžčņ]{2,15})?"))) {
+			surname = inputSurname;
+		}
+		else {
+			surname = "Unknown";
+		}
+	}
+	public void setPersonCode(String inputPersonCode) {
+		if((inputPersonCode != null) && (!inputPersonCode.isEmpty()) && (inputPersonCode.matches("[0-9]{6}[-]{1}[0-9]{5}"))) {
+			surname = inputPersonCode;
+		}
+		else {
+			surname = "Unknown";
+		}
+	}
+ 
 	
 	
 }
